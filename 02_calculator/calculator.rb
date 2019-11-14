db612a57bc9dc96c8a0b78f31c7b651b2e3ccb13
Singh <singhanga@ad.msoe.edu>
2019-11-13 21:13:18 -0600

    pushed all work done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
-#write your code here
+def add(a, b)
+    a+b
+end
+
+def subtract(a,b)
+    a-b
+end
+
+def sum(array)
+    sum = 0
+    array.each { |i| sum += i }
+    sum
+end
+
+def multiply(a,b)
+    mul = a*b
+    mul
+end
+
+def multiply(a,b,c)
+    mul = a*b*c
+    mul
+end
+
+def power(a, b)
+    a**b
+end
+
+def factorial(number)
+    fact = 1
+    index = fact
+    while (index > 0)
+        fact *= index
+        index -= 1
+    end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,37 @@
-#write your code here
+def echo (string)
+  string
+end
+
+def shout (string)
+  string.upcase
+end
+
+def repeat (string, n=2)
+  repeated = string
+  (n-1).times do
+    repeated += " #{string}"
+  end
+  return repeated
+end
+
+def start_of_word (string, num)
+  string[0, num]
+end
+
+def first_word (string)
+  string.split(' ')[0]
+end
+
+def titleize (string)
+  words = string.split
+  capitalised = []
+  words.each do |item|
+    if ["and", "the", "over", "a", "an"].include? item
+	 capitalised.push(item)
+	else
+	  capitalised.push(item.capitalize)
+	end
+  end
+  capitalised[0].capitalize!
+  capitalised.join(" ")
+end
